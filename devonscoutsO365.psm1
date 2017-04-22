8ead69bc142ca5d1d7034c45352bad773745688d
Richard Carpenter <artfulbodger@users.noreply.github.com>
2017-04-22 22:46:01 +0100

    Added E2 license detection and assignment if missing in New-dsO365User

--- devonscoutsO365.psm1
+++ devonscoutsO365.psm1
@@ -21,8 +21,6 @@
       Creates a new User Called John Doe with an alias of john.doe@devonscouts.org.uk
   #>
 
-
-
   [CmdletBinding()]
   [OutputType([int])]
   Param
@@ -45,8 +43,26 @@
     $upn = "$($firstname).$($lastname)@devonscouts.org.uk"
     
     Try {
-      Get-MsolUser -UserPrincipalName $upn -ErrorAction Stop
+      $user = Get-MsolUser -UserPrincipalName $upn -ErrorAction Stop
+      If ($user.IsLicensed -eq $false) {       
+        # Add license to existing O365 user
+        Set-MSOLUser -UserPrincipalName $upn -UsageLocation GB
+        Set-MsolUserLicense -UserPrincipalName $upn -LicenseOptions $licenseoptions
+      } else {
+        # Licensed - now check if license list contains E2 SKU
+        $licensedE2 = $false
+        Foreach ($license in $user.licenses) {
+          If ($license.AccountSkuId -eq 'devonscouts:STANDARDWOFFPACK') {
+            $licensedE2 = $true
+          }    
+        }
+        If ($licensedE2 -eq $false) {
+          # No E2 SKU license assigned
+          Set-MsolUserLicense -UserPrincipalName $upn -LicenseOptions $licenseoptions
+        }
+      }
     } catch [Microsoft.Online.Administration.Automation.MicrosoftOnlineException] {
+      # User not found - go ahead and create user with E2 SKU license
       New-MsolUser -UserPrincipalName $upn -FirstName $firstname -LastName $lastname -DisplayName $displayname -Password 'Scoutie1907' -ForceChangePassword $true -LicenseAssignment devonscouts:STANDARDWOFFPACK -LicenseOptions $licenseoptions -UsageLocation GB -Title $role
     }
   }
