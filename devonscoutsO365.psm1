4102716b46a0b824006c5e1d7602ff4f2acc267c
Richard Carpenter <artfulbodger@users.noreply.github.com>
2017-04-22 22:05:19 +0100

    Added check for existing user in Office 365 prior to attempting to create new user

--- devonscoutsO365.psm1
+++ devonscoutsO365.psm1
@@ -1,24 +1,24 @@
 ﻿function New-dsO365User
 {
   <#
-    .SYNOPSIS
-    Creates a new Office 365 user and licenses for base services.
+      .SYNOPSIS
+      Creates a new Office 365 user and licenses for base services.
 
-    .DESCRIPTION
-    Creates a new Office 365 user and grants a new E2 license with unrequire services disabled.
+      .DESCRIPTION
+      Creates a new Office 365 user and grants a new E2 license with unrequire services disabled.
 
-    .PARAMETER firstname
-    Users Firstname as defined on Compass.
+      .PARAMETER firstname
+      Users Firstname as defined on Compass.
 
-    .PARAMETER lastname
-    Users Lastname as defined on Compass.
+      .PARAMETER lastname
+      Users Lastname as defined on Compass.
 
-    .PARAMETER role
-    Users primary role as defined on Compass.
+      .PARAMETER role
+      Users primary role as defined on Compass.
 
-    .EXAMPLE
-    New-dsO365User -firstname 'John' -lastname 'Doe' -role 'County Commissioner'
-    Creates a new User Called John Doe with an alias of john.doe@devonscouts.org.uk
+      .EXAMPLE
+      New-dsO365User -firstname 'John' -lastname 'Doe' -role 'County Commissioner'
+      Creates a new User Called John Doe with an alias of john.doe@devonscouts.org.uk
   #>
 
 
@@ -44,7 +44,11 @@
     $displayname = "$($firstname) $($lastname)"
     $upn = "$($firstname).$($lastname)@devonscouts.org.uk"
     
-    New-MsolUser -UserPrincipalName $upn -FirstName $firstname -LastName $lastname -DisplayName $displayname -Password 'Scoutie1907' -ForceChangePassword $true -LicenseAssignment devonscouts:STANDARDWOFFPACK -LicenseOptions $licenseoptions -UsageLocation GB -Title $role
+    Try {
+      Get-MsolUser -UserPrincipalName $upn -ErrorAction Stop
+    } catch [Microsoft.Online.Administration.Automation.MicrosoftOnlineException] {
+      New-MsolUser -UserPrincipalName $upn -FirstName $firstname -LastName $lastname -DisplayName $displayname -Password 'Scoutie1907' -ForceChangePassword $true -LicenseAssignment devonscouts:STANDARDWOFFPACK -LicenseOptions $licenseoptions -UsageLocation GB -Title $role
+    }
   }
   End
   {
