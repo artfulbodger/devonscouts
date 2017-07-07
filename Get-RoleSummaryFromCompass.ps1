fd57aa20cdce9180115d8ff5d0fbeafdefc47d96
Richard Carpenter <artfulbodger@users.noreply.github.com>
2017-07-07 16:27:47 +0100

    Added new function to generate a count summary from the County Appointments Report

---
+++ Get-RoleSummaryFromCompass.ps1
@@ -0,0 +1,22 @@
+﻿#Requires -Version 3.0
+function Get-RoleSummaryFromCompass
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    Param
+    (
+        [string]$csvfile= 'C:\Users\Artfulbodger\Downloads\County Appointments Report v1.1.csv'
+    )
+
+    Begin
+    {
+      $appointments_list =  Import-Csv -Path $csvfile
+    }
+    Process
+    {
+      $appointments_list | group-object -Property Role | Select-Object Name,Count | Sort-Object Name | Export-Csv -Path 'C:\Users\Artfulbodger\Downloads\County Appointments Summary.csv' -NoTypeInformation
+    }
+    End
+    {
+    }
+}
