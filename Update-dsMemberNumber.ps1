460736a9aa1de8facf591abd32862cea1a56bbb2
Richard Carpenter <artfulbodger@users.noreply.github.com>
2017-11-05 20:18:19 +0000

    Added function to update Membership number to AzureAD user

---
+++ Update-dsMemberNumber.ps1
@@ -0,0 +1,40 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Update-dsMemberNumber
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        [Parameter(Mandatory)][ValidateScript({Test-Path $_ -PathType 'Leaf'})] $csvfile
+    )
+
+    Begin
+    {
+        Try {
+                Get-AzureADTenantDetail -ErrorAction Stop | Out-null
+            } Catch {
+                Connect-AzureAD
+            }
+        $memberlist= Import-csv -Path $csvfile
+    }
+    Process
+    {
+        Foreach($member in $memberlist) {
+            Write-Verbose "Updating $($member.upn) to $($member.number)"
+            Set-AzureADUserExtension -ObjectId $member.upn -ExtensionName "extension_f78b8b10d59f499ca99da2acdc29191b_membership_number" -ExtensionValue $member.number
+        }
+    }
+    End
+    {
+    }
+}
