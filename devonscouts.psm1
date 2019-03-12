847e3f68133d1ecc74b436c8acd580565630ef8e
Richard Carpenter <richard.carpenter@global.com>
2019-03-12 21:36:24 +0000

    Change Exported Functions
    * New module layout with Private Functions
    * New module layout with Public functions
    * Updated Script Module to Export Public folder function only
    
    Fixes #38

--- devonscouts.psm1
+++ devonscouts.psm1
@@ -1,5 +1,22 @@
 ﻿#Dot source all functions in all ps1 files located in the module folder
-Get-ChildItem -Path $PSScriptRoot\*.ps1 -Exclude *.tests.ps1, *profile.ps1 |
-ForEach-Object {
-    . $_.FullName
+#Get-ChildItem -Path $PSScriptRoot\*.ps1 -Exclude *.tests.ps1, *profile.ps1 |
+#ForEach-Object {
+#    . $_.FullName
+#}
+$functionFolders = @('Public', 'Internal', 'Classes')
+ForEach ($folder in $functionFolders)
+{
+    $folderPath = Join-Path -Path $PSScriptRoot -ChildPath $folder
+    If (Test-Path -Path $folderPath)
+    {
+        Write-Verbose -Message "Importing from $folder"
+        $functions = Get-ChildItem -Path $folderPath -Filter '*.ps1' 
+        ForEach ($function in $functions)
+        {
+            Write-Verbose -Message "  Importing $($function.BaseName)"
+            . $($function.FullName)
+        }
+    }    
 }
+$publicFunctions = (Get-ChildItem -Path "$PSScriptRoot\Public" -Filter '*.ps1').BaseName
+Export-ModuleMember -Function $publicFunctions
