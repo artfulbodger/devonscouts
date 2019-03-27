4db963239614e66cb5ee7a92b227e7bd8de2f106
Richard Carpenter <artfulbodger@users.noreply.github.com>
2019-03-27 18:46:33 +0000

    Add MailChimp functions
    Signed-off-by: Richard Carpenter <artfulbodger@users.noreply.github.com>

---
+++ New-dsUserMailChimpUpdateCard.ps1
@@ -0,0 +1,60 @@
+﻿function New-dsUserMailChimpUpdateCard
+{
+    [CmdletBinding()]
+    Param
+    (
+      [Parameter(Mandatory,HelpMessage='Previous Mailchimp list members Count')][string]$mcprevcount,
+      [Parameter(Mandatory,HelpMessage='Removed members count')][string]$removecount,
+      [Parameter(Mandatory,HelpMessage='Added members count')][string]$addcount,
+      [Parameter(Mandatory,HelpMessage='New Mailchimp list members Count')][string]$mcnewcount
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+      $Body = @{
+        text = 'List - Compass Adults'
+        title ='MailChimp Update'
+        themeColor='800080'
+        sections = @(
+          @{
+            activityTitle = 'Members added and removed'
+            activityText = "$(Get-Date -format 'dd/MM/yyyy HH:mm')"
+          }
+          @{
+            title = 'Update Summary'
+            facts = @(
+              @{
+                name = 'Members removed'
+                value = $removecount
+              }
+              @{
+                name = 'Members added'
+                value = $addcount
+              }
+              @{
+                name = 'Previous count'
+                value = $mcprevcount
+              }
+              @{
+                name = 'New count'
+                value = $mcnewcount
+              }
+            )
+          }
+        )
+      } | ConvertTo-Json -Depth 5
+
+      $uri = 'https://outlook.office.com/webhook/4ad2c863-4658-4dc1-ae97-cdc211e1547d@1351267d-8d51-4e96-9069-b13305364b74/IncomingWebhook/7e8f1795df5641cab32beaf1697fb5fc/d853adbe-381e-4400-a375-a75f731c0423'
+      $contenttype = 'application/json'
+      Try {
+        Invoke-RestMethod -Method Post -uri $uri -body $body -ContentType $contenttype
+      } Catch {
+      }
+    }
+    End
+    {
+    }
+}
---
+++ Remove-dsMailchimpMember.ps1
@@ -0,0 +1,41 @@
+﻿function Remove-dsMailchimpMember
+{
+  
+    [CmdletBinding()]
+    [OutputType([int])]
+    Param
+    (
+      [Parameter(Mandatory,HelpMessage='Email address to be deleted from Mailchimp List')][string]$emailaddress,
+      [string]$listID = 'a512378808',
+      [string]$mailchimpAPIKey = 'e8197bfcdd50c5ba7ea60786daabcc88-us13',
+      [string]$dc='us13'
+    )
+
+    Begin
+    {
+      $user = 'anystring'
+      $pair = "${user}:${mailchimpAPIKey}"
+      $bytes = [Text.Encoding]::ASCII.GetBytes($pair)
+      $base64 = [Convert]::ToBase64String($bytes)
+      $basicAuthValue = ('Basic {0}' -f $base64)
+      $headers = @{ Authorization = $basicAuthValue }
+      
+      # Email MD5 hash
+      $md5 = new-object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+      $utf8 = new-object -TypeName System.Text.UTF8Encoding
+      $emailMD5 = [BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($emailaddress)))
+      $emailMD5 = $emailMD5.ToLower() -replace '-',''
+    }
+    Process
+    {
+      Try {
+        $response = Invoke-WebRequest -Uri "https://$($dc).api.mailchimp.com/3.0/lists/$listID/members/$emailMD5" -Headers $headers -Method Delete
+      } Catch {
+        $response = $_.ErrorDetails.Message | ConvertFrom-Json
+        Write-Verbose -Message "$emailaddress - $($response.detail)"
+      }
+    }
+    End
+    {
+    }
+}
